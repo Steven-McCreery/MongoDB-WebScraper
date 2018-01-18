e5d5c39417f37719dad54b3ad5a1178c12dc1138
Steven McCreery <logad8@gmail.com>
2018-01-18 16:42:52 -0500

    changing file structure so that mvc is better represented. starting handlebars structure creation

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
 // loading dependencies
-import express from "express";
-import bodyParser from "body-parser";
-import logger from "morgan";
-import mongoose from "mongoose";
-import axios from "axios";
-import cheerio from "cheerio";
-import db from "./models";
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+var db = require("./model");
 
 // setting up port
 const PORT = 3000;
@@ -14,12 +15,18 @@ const PORT = 3000;
 // setting up express
 const app = express();
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended:false }));
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // setting up mongoose and mongoose with ecma2015 promises
+mongoose.connect("mongodb://localhost:27017");
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost")
 
+// setting up routes
+var routes = require("./controller/scraper.js");
+// app.use("/", routes);
 
-
+// starting up server and listening to port
+app.listen(PORT, () => console.log(`Scraper running on port: ${PORT}, Mongo Time!`));
