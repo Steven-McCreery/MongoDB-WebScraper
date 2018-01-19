567acf06fc598a55d26d737e7f862c63f8c339b0
Steven McCreery <logad8@gmail.com>
2018-01-19 14:44:11 -0500

    have basic handlebars page rendering

--- controller/scraper.js
+++ controller/scraper.js
@@ -5,13 +5,16 @@ var router = express.Router();
 var scrape = require("../model")
 
 router.get("/", function(req, res) {
-	scrape.all(function(data) {
-		var hbsObject = {
-			articles: data
-		};
-		console.log(hbsObject);
-		res.render("index", hbsObject);
-	})
+
+	res.render("index");
+
+	// scrape.all(function(data) {
+	// 	var hbsObject = {
+	// 		articles: data
+	// 	};
+	// 	console.log(hbsObject);
+	// 	res.render("index", hbsObject);
+	// })
 })
 
 
